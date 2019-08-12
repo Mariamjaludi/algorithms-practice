7ae830f7483f8dae2dd715dc3bf35555db60e571
mariamjaludi <mariam.jaludi@gmail.com>
2019-08-12 19:18:28 +0300

    syntax clean

--- twoSum.rb
+++ twoSum.rb
@@ -5,40 +5,13 @@ target = 9
 
 
 def two_sum(nums, target)
-
   return [] if nums.length < 2
-
   hash = {}
-
   for i in (0..nums.length - 1)
-      return [hash[target - nums[i]], i] if (hash[target - nums[i]] && hash[target - nums[i]] != i)
-
-      hash[nums[i]] = i
+    return [hash[target - nums[i]], i] if (hash[target - nums[i]] && hash[target - nums[i]] != i)
+    hash[nums[i]] = i
   end
-
   []
-
-end
-
-  # i = 0
-  # answer = []
-  # return answer if nums.length < 2
-  #
-  # while i < nums.length
-  #   diff = target - nums[i]
-  #   # binding.pry
-  #   j = i + 1
-  #   while j < nums.length
-  #       if (nums[j] == diff)
-  #         # binding.pry
-  #           answer[0] = i
-  #           answer[1] = j
-  #       end
-  #       j += 1
-  #   end
-  #   i += 1
-  # end
-  # answer
 end
 
 puts two_sum(arr, target)
