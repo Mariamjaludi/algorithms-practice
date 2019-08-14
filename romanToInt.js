0d2056b760316a34fce58d5610dc8e07b8290c92
mariamjaludi <mariam.jaludi@gmail.com>
2019-08-14 13:23:20 +0300

    faster romanToInt

--- romanToInt.js
+++ romanToInt.js
@@ -11,8 +11,18 @@ const romanToInt = s => {
     ['L', 50],
     ['C', 100],
     ['D', 500],
-    ['M', 1000],
+    ['M', 1000]
   ]);
+
+  // const map = {
+  //   I: 1,
+  //   V: 5,
+  //   X: 10,
+  //   L: 50,
+  //   C: 100,
+  //   D: 500,
+  //   M: 1000
+  // };
 
   let i = s.length - 1;
   let result = map.get(s[i]);
