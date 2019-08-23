7a51033ad8ffdf2eeb5c6dc210fc571a9df34322
mariamjaludi <mariam.jaludi@gmail.com>
2019-08-23 16:18:53 +0300

    sort by parity

--- sortArrayByParity.py
+++ sortArrayByParity.py
@@ -1,6 +1,4 @@
 # Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
-
-
 class Solution(object):
     def sortArrayByParity(self, A):
         left = 0
