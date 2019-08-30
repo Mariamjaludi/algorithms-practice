6068c5232f8c50f7f760c8b51b5012e00b634ac2
mariamjaludi <mariam.jaludi@gmail.com>
2019-08-30 15:01:29 -0700

    insert, find, printInOrder, bfs

--- tree.py
+++ tree.py
@@ -4,4 +4,35 @@ class Node:
         self.left = None
         self.right = None
 
-        
+    def insert(self, value):
+        if value <= self.value:
+            if self.left == None:
+                self.left = Node(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right == None:
+                self.right = Node(value)
+            else:
+                self.right.insert(value)
+
+    def find(self, value):
+        if value == self.value:
+            return True
+        elif value < self.value:
+            if self.left == None:
+                return False
+            else:
+                return self.left.find(value)
+        else:
+            if self.right == None:
+                return False
+            else:
+                return self.right.find(value)
+
+    def printInOrder(self):
+        if self.left != None:
+            self.left.printInOrder()
+        print(self.data)
+        if self.right != None:
+            self.right.printInOrder()
