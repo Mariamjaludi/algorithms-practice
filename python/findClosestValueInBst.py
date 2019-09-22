c00226320beec6dd6f964211dfff82903670fa0e
mariamjaludi <mariam.jaludi@gmail.com>
2019-09-21 17:27:23 -0700

    closest value in bst

--- python/findClosestValueInBst.py
+++ python/findClosestValueInBst.py
@@ -1,16 +1,16 @@
 def findClosestValueInBst(tree, target):
     # Write your code here.
-	closest = float('inf')
-	current = tree
-	while current is not None:
-		currentV = current.value
-		diff = abs(currentV - target)
-		if diff < abs(closest - target):
-			closest = current.value
-		if currentV == target:
-			return current.value
-		elif currentV < target:
-			current = current.right
-		elif currentV > target:
-			current = current.left
-	return closest
+    closest = float('inf')
+    current = tree
+    while current is not None:
+        currentV = current.value
+        diff = abs(currentV - target)
+        if diff < abs(closest - target):
+            closest = current.value
+        if currentV == target:
+            return currentV
+        elif currentV < target:
+            current = current.right
+        elif currentV > target:
+            current = current.left
+    return closest
