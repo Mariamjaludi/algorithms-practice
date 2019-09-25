7de36e6f0a0a0d2c0812149e0382677e0f33eb91
mariamjaludi <mariam.jaludi@gmail.com>
2019-09-25 14:11:01 -0700

    merge sort

--- python/mergeSort.py
+++ python/mergeSort.py
@@ -3,7 +3,10 @@ def mergeSort(array):
     # Base case:
     if len(array) == 1:
         return array
-    middleIndex = len(array) // 2 # find the middle (rounded down)
+    middleIndex = len(array) // 2  # find the middle (rounded down)
     leftHalf = array[:middleIndex]
     rightHalf = array[middleIndex:]
-    return mergerSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf))
+    return mergeSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf))
+
+def mergeSortedArrays(leftHalf, rightHalf):
+    pass
