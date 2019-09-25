555afca38260237bfdafb039f9288e94dd61a7a5
mariamjaludi <mariam.jaludi@gmail.com>
2019-09-25 14:18:10 -0700

    finished mergeSort

--- python/mergeSort.py
+++ python/mergeSort.py
@@ -9,4 +9,22 @@ def mergeSort(array):
     return mergeSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf))
 
 def mergeSortedArrays(leftHalf, rightHalf):
-    pass
+    sortedArray = [None] * (len(leftHalf) + len(rightHalf))
+    i = j = k = 0
+    while i < len(leftHalf) and j < len(rightHalf):
+        if leftHalf[i] < rightHalf[j]:
+            sortedArray[k] = leftHalf[i]
+            i += 1
+        else:
+            sortedArray[k] = rightHalf[j]
+            j += 1
+        k += 1
+    while i < len(leftHalf):
+        sortedArray[k] = leftHalf[i]
+        i += 1
+        k += 1
+    while j < len(rightHalf):
+        sortedArray[k] = rightHalf[j]
+        j += 1
+        k += 1
+    return sortedArray
