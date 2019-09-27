0aa267da02b0c0e9ec0b6911ad5e153d7e12ec8b
mariamjaludi <mariam.jaludi@gmail.com>
2019-09-27 11:50:26 -0700

    youngest anscestor

--- python/youngestCommonAncestor.py
+++ python/youngestCommonAncestor.py
@@ -5,35 +5,36 @@
 # Write a function that returns the youngest common ancestor to the two
 # descendants.
 
+
 def getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo):
     # Write your code here.
-	d1depth = findDepth(descendantOne, topAncestor)
-	d2depth = findDepth(descendantTwo, topAncestor)
-	diff = abs(d1depth - d2depth)
-	if d1depth > d2depth:
-			descendantOne = levelDescendants(descendantOne, diff)
-	elif d1depth < d2depth:
-			descendantTwo = levelDescendants(descendantTwo, diff)
+    d1depth = findDepth(descendantOne, topAncestor)
+    d2depth = findDepth(descendantTwo, topAncestor)
+    diff = abs(d1depth - d2depth)
+    if d1depth > d2depth:
+        descendantOne = levelDescendants(descendantOne, diff)
+    elif d1depth < d2depth:
+        descendantTwo = levelDescendants(descendantTwo, diff)
 
+    while descendantOne != topAncestor and descendantTwo != topAncestor:
+        if descendantOne == descendantTwo:
+            return descendantOne
+        else:
+            descendantOne = descendantOne.ancestor
+            descendantTwo = descendantTwo.ancestor
+    return topAncestor
 
-	while descendantOne != topAncestor and descendantTwo != topAncestor:
-		if descendantOne == descendantTwo:
-			return descendantOne
-		else:
-			descendantOne = descendantOne.ancestor
-			descendantTwo = descendantTwo.ancestor
-	return topAncestor
 
 def findDepth(node, topAncestor):
-	depth = 0
-	while node != topAncestor:
-		node = node.ancestor
-		depth += 1
-	return depth
+    depth = 0
+    while node != topAncestor:
+        node = node.ancestor
+        depth += 1
+    return depth
+
 
 def levelDescendants(descendant, difference):
-	while difference > 0:
-		descendant = descendant.ancestor
-		difference -= 1
-	return descendant
-		
+    while difference > 0:
+        descendant = descendant.ancestor
+        difference -= 1
+    return descendant
