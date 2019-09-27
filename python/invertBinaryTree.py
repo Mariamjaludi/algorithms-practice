8df527a453ad2d7dd353d8db490bbacccb4eab38
mariamjaludi <mariam.jaludi@gmail.com>
2019-09-26 19:42:09 -0700

    iterative invert binary tree

--- python/invertBinaryTree.py
+++ python/invertBinaryTree.py
@@ -1,7 +1,22 @@
 def invertBinaryTree(tree):
     if tree is None:
         return
-    tree.left, tree.right = tree.right, tree.left
+    swapLeftAndRight(tree)
     invertBinaryTree(tree.left)
     invertBinaryTree(tree.right)
-    return tree    
+    return tree
+
+
+def invertBinaryTreeIterative(tree):
+    queue = [tree]
+    while len(queue):
+        current = queue.pop(0)
+        if current is None:
+            continue
+        swapLeftAndRight(current)
+        queue.append(current.left)
+        queue.append(current.right)
+
+
+def swapLeftAndRight(tree):
+    tree.left, tree.right = tree.right, tree.left
