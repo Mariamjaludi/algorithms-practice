e9caa1cf441bff2cb778f0e0a780cda3689e1994
mariamjaludi <mariam.jaludi@gmail.com>
2019-10-02 12:58:53 -0700

    suffix trie

---
+++ python/populateSuffixTrie.py
@@ -0,0 +1,25 @@
+class SuffixTrie:
+    def __init__(self, string):
+        self.root = {}
+        self.endSymbol = "*"
+        self.populateSuffixTrie(string)
+
+    def populateSuffixTrie(self, string):
+        for i in range(len(string)):
+            insertSubstringStartingAt(i, string)
+
+    def insertSubstringStartingAt(self, i, string):
+        node = self.root
+        for j in range(i, len(string)):
+            if string[j] not in node:
+                node[string[j]] = {}
+            node = node[string[j]]
+        node[self.endSymbol] = True
+
+    def contains(self, string):
+        node = self.root
+        for letter in string:
+            if letter not in node:
+                return False
+            node = node[letter]
+        return self.endSymbol in Node    
