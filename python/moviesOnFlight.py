b70cbf9f29e3c663e14a8a675a2b6d7e7f8d5f56
mariamjaludi <mariam.jaludi@gmail.com>
2019-10-04 19:35:47 -0700

    movies on flight

---
+++ python/moviesOnFlight.py
@@ -0,0 +1,12 @@
+# Movies On Flight
+# You are on a flight and wanna watch two movies during this flight. You are
+# given int[] movie_duration which includes all the movie durations. You are
+# also given the duration of the flight which is d in minutes. Now, you need to
+# pick two movies and the total duration of the two movies is less than or equal
+# to (d - 30min). Find the pair of movies with the longest total duration. If
+# multiple found, return the pair with the longest movie.
+
+movie_duration = [90, 85, 75, 60, 120, 150, 125]
+d = 250
+maxMovietime = d - 30
+ans = [90, 125]
