63954890a9ee0aa34d33c30bc74395669f8b3b81
mariamjaludi <mariam.jaludi@gmail.com>
2019-10-04 19:48:39 -0700

    done

--- python/moviesOnFlight.py
+++ python/moviesOnFlight.py
@@ -8,5 +8,31 @@
 
 movie_duration = [90, 85, 75, 60, 120, 150, 125]
 d = 250
-maxMovietime = d - 30
+
 ans = [90, 125]
+
+def movies(movie_duration, d):
+    if len(movie_duration) == 0:
+        return []
+
+    maxMovietime = d - 30
+    movie_duration.sort()
+    left = 0
+    right = len(movie_duration) - 1
+    
+    dec = 0
+
+    while left < right:
+        currentSum = movie_duration[left] + movie_duration[right]
+        if currentSum == maxMovietime + dec:
+            return [movie_duration[left], movie_duration[right]]
+        elif currentSum > maxMovietime + dec:
+            right -= 1
+        elif currentSum < maxMovietime + dec:
+            left += 1
+        if left >= right:
+            dec -= 1
+            left = 0
+            right = len(movie_duration) - 1
+
+print(movies(movie_duration, d))
