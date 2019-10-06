8bafba890ddefb214ab43c5893d22e3e1ee6907a
mariamjaludi <mariam.jaludi@gmail.com>
2019-10-05 23:16:22 -0700

    fixed heap bugs

--- python/heap.py
+++ python/heap.py
@@ -1,4 +1,4 @@
-class MinHeap
+class MinHeap:
     def __init__(self, array):
         self.heapList = array
         self.length = len(array)
@@ -10,14 +10,15 @@ class MinHeap
         self.bubbleUp(i)
 
     def bubbleUp(self, i):
-        parentIdx = (i - 2) // 2
+        parentIdx = abs(i - 1) // 2
         while i > 0 and self.heapList[i] < self.heapList[parentIdx]:
-            i, parentIdx = self.swap(self.heapList[i], self.heapList[parentIdx], i, parentIdx)
+            i, parentIdx = self.swap(
+                self.heapList[i], self.heapList[parentIdx], i, parentIdx)
 
     def swap(self, a, b, i, parentIdx):
         a, b = b, a
         i = parentIdx
-        parentIdx = (i - 2) // 2
+        parentIdx = (i - 1) // 2
         return i, parentIdx
 
     def pop(self):
@@ -29,15 +30,15 @@ class MinHeap
 
     def bubbleDown(self, i=0):
         val = self.heapList[i]
-        leftIdx = 2*i + 1
-        rightIdx = 2*i + 2
+        leftIdx = 2 * i + 1
+        rightIdx = 2 * i + 2
         children = self.getChildren(leftIdx, rightIdx)
         smaller, sI = self.getSmaller(children)
         while children and val > smaller:
-            self.heapList[i], self.heapList[iS] = self.heapList[sI], self.heapList[i]
+            self.heapList[i], self.heapList[sI] = self.heapList[sI], self.heapList[i]
             i = sI
-            leftIdx = 2*i + 1
-            rightIdx = 2*i + 2
+            leftIdx = 2 * i + 1
+            rightIdx = 2 * i + 2
             children = self.getChildren(leftIdx, rightIdx)
             smaller, sI = self.getSmaller(children)
 
@@ -53,14 +54,14 @@ class MinHeap
         if len(children) == 0:
             return float('inf'), None
         if len(children) == 1:
-           return children[0]
+            return children[0]
         else:
             childA, iA = children[0]
             childB, iB = children[1]
             return children[0] if childA < childB else children[1]
 
 
-class MaxHeap
+class MaxHeap:
     def __init__(self, array):
         self.heapList = array
         self.length = len(array)
@@ -72,34 +73,36 @@ class MaxHeap
         self.bubbleUp(i)
 
     def bubbleUp(self, i):
-        parentIdx = (i - 2) // 2
+        parentIdx = abs(i - 1) // 2
         while i > 0 and self.heapList[i] > self.heapList[parentIdx]:
-            i, parentIdx = self.swap(self.heapList[i], self.heapList[parentIdx], i, parentIdx)
+            i, parentIdx = self.swap(i, parentIdx)
 
-    def swap(self, a, b, i, parentIdx):
-        a, b = b, a
+    def swap(self, i, parentIdx):
+        self.heapList[i], self.heapList[parentIdx] = self.heapList[parentIdx], self.heapList[i]
         i = parentIdx
-        parentIdx = (i - 2) // 2
+        parentIdx = (i - 1) // 2
         return i, parentIdx
 
     def pop(self):
         top = self.heapList[0]
         last = self.heapList[-1]
         self.heapList[0] = last
+        self.heapList.pop()
+        self.length -= 1
         self.bubbleDown()
         return top
 
     def bubbleDown(self, i=0):
         val = self.heapList[i]
-        leftIdx = 2*i + 1
-        rightIdx = 2*i + 2
+        leftIdx = 2 * i + 1
+        rightIdx = 2 * i + 2
         children = self.getChildren(leftIdx, rightIdx)
         bigger, bIdx = self.getBigger(children)
         while children and val < bigger:
             self.heapList[i], self.heapList[bIdx] = self.heapList[bIdx], self.heapList[i]
-            i = iS
-            leftIdx = 2*i + 1
-            rightIdx = 2*i + 2
+            i = bIdx
+            leftIdx = 2 * i + 1
+            rightIdx = 2 * i + 2
             children = self.getChildren(leftIdx, rightIdx)
             bigger, bIdx = self.getBigger(children)
 
@@ -115,7 +118,7 @@ class MaxHeap
         if len(children) == 0:
             return float('-inf'), None
         if len(children) == 1:
-           return children[0]
+            return children[0]
         else:
             childA, iA = children[0]
             childB, iB = children[1]
